f7a67b925d5a8f260c75ced87a5cedd05dfc2e0b
Alexandra Dumitrachi <dumitrachi.alexandra@yahoo.com>
2020-07-21 01:29:31 +0300

    last changes

---
+++ tabs/contact-us/contact-us.js
@@ -0,0 +1,32 @@
+console.log("It works");
+
+$(document).ready(function () {
+  $(".submit").click(function (event) {
+    let email = $(".email").val();
+    let subject = $(".subject").val();
+    let message = $(".message").val();
+    let statusElm = $(".status");
+    statusElm.empty();
+
+    if (email.length > 5 && email.includes("@") && email.includes(".")) {
+      console.log("ok");
+    } else {
+      event.preventDefault();
+      statusElm.append("<div>Email is not valid</div>");
+    }
+
+    if (subject.length >= 2) {
+      console.log("ok");
+    } else {
+      event.preventDefault();
+      statusElm.append("<div>Subject is not valid</div>");
+    }
+
+    if (message.length >= 10) {
+      console.log("ok");
+    } else {
+      event.preventDefault();
+      statusElm.append("<div>Message is not valid</div>");
+    }
+  });
+});
